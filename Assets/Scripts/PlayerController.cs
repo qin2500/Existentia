52b6f34c10e4daf49c063dd4f861a43ca6bc4e93
qin2500 <52422134+qin2500@users.noreply.github.com>
2021-08-19 16:48:43 -0400

    Added Jumping and Drag Force to Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //Camera Rotation
+      //Camera Rotation
     [Range(0f, 300f)] public float mouseSensitivity;
     public Transform cameraTransform;
     private float _xRotation = 0f;
@@ -14,8 +15,17 @@ public class PlayerController : MonoBehaviour
     public float extraGravity = 10f;
     public float maxVelocity;
     public float moveSpeed = 400;
+    public float maxSlopeAngle = 20f;
 
+    public Transform goundCheck;
+    public float groundCheckRadius;
     public LayerMask groundLayer;
+    public bool grounded = true;
+    public bool canJump = true;
+    public float jumpCoolDown;
+    public float jumpForce;
+    public float dragForce;
+    
     
     void Start()
     {
@@ -30,8 +40,8 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        CheckGround();
         Move();
-        
     }
 
     private void CameraLook()
@@ -54,11 +64,19 @@ public class PlayerController : MonoBehaviour
         float y = Input.GetAxisRaw("Vertical");
 
         Vector2 magnitude = FindRelativeVelocity();
+        counterForce(x,y,magnitude);
+        
+        if (Input.GetButton("Jump")) Jump();
+        float mutiplier = 1;
 
+        if (!grounded) mutiplier = 0.5f;
+        
+        
+        
         if (magnitude.magnitude < maxVelocity)
         {
-            rb.AddForce(this.transform.forward*y*moveSpeed*Time.deltaTime);
-            rb.AddForce(this.transform.right*x*moveSpeed*Time.deltaTime);
+            rb.AddForce(this.transform.forward*y*moveSpeed*Time.deltaTime * mutiplier);
+            rb.AddForce(this.transform.right*x*moveSpeed*Time.deltaTime * mutiplier);
         }
         
     }
@@ -77,4 +95,53 @@ public class PlayerController : MonoBehaviour
         return new Vector2(xMag, yMag);
         
     }
-}
+
+    public void Jump()
+    {
+        if (grounded && canJump)
+        {
+            canJump = false;
+            rb.AddForce(Vector3.up*jumpForce);
+            Invoke(nameof(resetJump),jumpCoolDown);
+        }
+    }
+
+    private bool IsFloor(Vector3 v) {
+        float angle = Vector3.Angle(Vector3.up, v);
+        return angle < maxSlopeAngle;
+    }
+
+    private bool _cancellingGrounded;
+
+    private void CheckGround()
+    {
+        grounded = Physics.CheckSphere(goundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    private void StopGrounded() {
+        grounded = false;
+    }
+
+    private void resetJump()
+    {
+        canJump = true;
+    }
+
+    public void counterForce(float x, float y, Vector2 mag)
+    {
+        if (!grounded) return;
+        
+        var threshold = 0.01;
+        if (Math.Abs(mag.x) > threshold && Math.Abs(x) < 0.05f || (mag.x < -threshold && x > 0) || (mag.x > threshold && x < 0)) {
+            rb.AddForce(moveSpeed * transform.right * Time.deltaTime * -mag.x * dragForce);
+        }
+        if (Math.Abs(mag.y) > threshold && Math.Abs(y) < 0.05f || (mag.y < -threshold && y > 0) || (mag.y > threshold && y < 0)) {
+            rb.AddForce(moveSpeed * transform.forward * Time.deltaTime * -mag.y * dragForce);
+        }
+
+//        if (x == 0 && y == 0)
+//        {
+//            rb.AddForce(rb.velocity * Time.deltaTime * dragForce * -1);
+//        }
+    }
+    }
